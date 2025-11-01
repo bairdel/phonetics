d951972465506c0fbf4104d61868c71a8a3e0edc
bairdel <bairdel@outlook.com>
2025-11-01 19:41:31 +0000

    trying to deploy on render

--- phonetics/__init__.py
+++ phonetics/__init__.py
@@ -2,8 +2,22 @@ from flask import Flask
 
 from phonetics import pages
 
+class WSGILoggingMiddleware:
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        print(f"Incoming request: {environ['REQUEST_METHOD']} {environ['PATH_INFO']}")
+        return self.app(environ, start_response)
+
 def create_app():
     app = Flask(__name__)
-
+    app.wsgi_app = WSGILoggingMiddleware(app.wsgi_app)  # Applying WSGI middleware
     app.register_blueprint(pages.bp)
     return app
+
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
---
+++ phonetics/wsgi.py
@@ -0,0 +1,5 @@
+from phonetics import create_app
+
+if "name" == "__main__":
+    app = create_app()
+    app.run()
